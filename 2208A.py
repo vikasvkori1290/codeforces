a92eba8d0c114ab46f6ed4a2fbac2de8f446398f
vikasvkori1290 <vikasvkori129@gmail.com>
2026-07-24 22:13:06 +0530

    24-07-2026

---
+++ 2208A.py
@@ -0,0 +1,22 @@
+t=int(input())
+for i in range(t):
+    n = int(input())
+
+    matrix = []
+    for i in range(n):
+        row = list(map(int, input().split()))
+        matrix.append(row)
+    freq={}
+    for i in range(n):
+        for j in range(n):
+            value = matrix[i][j]
+
+            if value in freq:
+                freq[value] += 1
+            else:
+                freq[value] = 1
+    maxi = max(freq.values())
+    if maxi>(n*n)-n:
+        print("NO")
+    else:
+        print("YES")    
