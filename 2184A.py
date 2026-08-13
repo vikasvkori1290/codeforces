423247b30a5b88648c7d87f486e2730cf2df7382
vikasvkori1290 <vikasvkori129@gmail.com>
2026-08-13 23:37:05 +0530

    13-08-46

--- 2184A.py
+++ 2184A.py
@@ -1,8 +1,11 @@
 t=int(input())
 for i in range(t):
     n=int(input())
-    if n%3==0:
-        total=n//3
-    elif n%3==2:
-        total=n//2
-    
+    if n == 2:
+        print(2)
+    elif n == 3:
+        print(3)
+    elif n % 2 == 0:
+        print(0)
+    else:
+        print(1)
